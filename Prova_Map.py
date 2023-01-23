265e71b0faa8390ad326c1dad8b57e825c30e9c2
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2023-01-23 21:48:42 +0100

    Update Prova_Map.py

--- Prova_Map.py
+++ Prova_Map.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-#import plotly.graph_objects as go
-import chart_studio.plotly as p
+import plotly.graph_objects as go
 
 #simply plotting a map with no parameters
 #st.map()
